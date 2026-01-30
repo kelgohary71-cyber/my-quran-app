85dbb70a95cc4927aa0f1a4470a446c56950a117
kelgohary71-cyber <kelgohary71@gmail.com>
2026-01-30 19:17:44 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'quran-app-v22-offline';
+
+const FILES_TO_CACHE = [
+    './',
+    './index.html',
+    './logo.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response =>
+            response ||
+            fetch(event.request).then(fetchRes => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, fetchRes.clone());
+                    return fetchRes;
+                });
+            })
+        )
+    );
+});
