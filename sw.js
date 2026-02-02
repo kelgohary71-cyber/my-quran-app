693acd7b1d278767f2aa3155f153828c41836cfc
kelgohary71-cyber <kelgohary71@gmail.com>
2026-02-02 14:08:07 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'quran-app-v33';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
